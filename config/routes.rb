717d3c49f8f025ee0463c137fd9373d353eecda5
hans bousquet lafond <hans.bl@icloud.com>
2022-07-05 20:41:06 +0100

    Controllers OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get 'restaurants', to: 'restaurants#index'
