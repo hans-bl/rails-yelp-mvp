46963506f2f626cff6d2d55c78c5f1859b7281b4
hans bousquet lafond <hans.bl@icloud.com>
2022-07-05 20:31:20 +0100

    Seeding and routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurant', to: 'restaurants#show'
+
+  # get 'restaurant/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
