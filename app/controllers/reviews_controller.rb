6c9c380afd99514783615ba571b1ba43aa1ca778
hans bousquet lafond <hans.bl@icloud.com>
2022-07-07 18:34:03 +0100

    Yelp MVP OK

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,14 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
